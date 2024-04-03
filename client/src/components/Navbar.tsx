37089965c451417260312c209f930081cfaadf09
pnguy118 <phucnhat2111@gmail.com>
2024-04-03 14:58:35 -0400

    update navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -7,9 +7,9 @@ const Navbar = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             COMP309-Group2
           </Typography>
-          <Button color="inherit">Home</Button>
-          <Button color="inherit">Data Exploration</Button>
-          <Button color="inherit">GitHub</Button>
+          <Button color="inherit" href='/'>Home</Button>
+          <Button color="inherit" href='https://data.torontopolice.on.ca/datasets/a89d10d5e28444ceb0c8d1d4c0ee39cc_0/explore?location=15.927605%2C-28.347773%2C2.59' target='_blank'>Data Exploration</Button>
+          <Button color="inherit" href='https://github.com/TariqueJemison01/Toronto-Bike-Theft-Predictive-Model' target='_blank'>Github</Button>
         </Toolbar>
       </AppBar>
     </Box>
